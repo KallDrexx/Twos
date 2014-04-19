d5a659fc96a775f1f1e73f57e32bdb7cf60050de
KallDrexx <me@mshapiro.net>
2014-04-19 19:25:31 -0400

    Added initial game models

---
+++ Src/Twos/Models/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twos.Models
+{
+    public class GameState
+    {
+        private readonly int[,] _board = new int[4,4];
+        private readonly LinkedList<GameAction> _actions = new LinkedList<GameAction>(); 
+
+        public int Score { get; set; }
+        public LinkedList<GameAction> Actions { get { return _actions; } }
+        public int[,] Board { get { return _board; } }
+    }
+}
---
+++ Src/Twos/Processors/GameActionProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twos.Processors
+{
+    public class GameActionProcessor
+    {
+        private readonly Random _random;
+
+        public int Seed { get; private set; }
+
+        public GameActionProcessor(int? seed = null)
+        {
+            if (seed == null)
+                seed = new Random().Next();
+
+            _random = new Random(seed.Value);
+            Seed = seed.Value;
+        }
+
+
+        
+    }
+}
