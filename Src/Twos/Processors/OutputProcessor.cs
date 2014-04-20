390fa8116541e24f7da9330b861e719b3a62fb9a
KallDrexx <me@mshapiro.net>
2014-04-19 21:05:51 -0400

    Added output processor to render game board

---
+++ Src/Twos/Processors/OutputProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twos.Models;
+
+namespace Twos.Processors
+{
+    public class OutputProcessor
+    {
+        private const int TileDisplayWidth = 5;
+
+        public void DisplayGame(GameState state)
+        {
+            if (state == null)
+                throw new ArgumentNullException("state");
+
+            Console.Clear();
+            
+            DisplayBoard(state.Board);
+        }
+
+        private void DisplayBoard(int[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int column = 0; column < board.GetLength(1); column++)
+                {
+                    int positionX = (row * TileDisplayWidth);
+                    int positionY = column;
+
+                    DisplayTileValue(positionX, positionY, board[row, column]);
+                }
+            }
+        }
+
+        private void DisplayTileValue(int startX, int startY, int value)
+        {
+            int digitCount = GetNumberOfDigits(value);
+            startX += (TileDisplayWidth - digitCount);
+
+            Console.SetCursorPosition(startX, startY);
+            Console.Write(value);
+        }
+
+        private int GetNumberOfDigits(int value)
+        {
+            int digits = 0;
+            while ((value = value % 10) > 0)
+                digits++;
+
+            return digits;
+        }
+    }
+}
--- Src/Twos/Program.cs
+++ Src/Twos/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Twos.Models;
+using Twos.Processors;
 
 namespace Twos
 {
@@ -10,6 +12,10 @@ namespace Twos
     {
         static void Main(string[] args)
         {
+            var state = new GameState();
+            var output = new OutputProcessor();
+
+            output.DisplayGame(state);
         }
     }
 }
