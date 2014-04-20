cd7590c7935f6b9a24d457dce0816801c3c5efef
KallDrexx <me@mshapiro.net>
2014-04-20 12:09:58 -0400

    Added win condition checking

--- Src/Twos/Processors/GameActionProcessor.cs
+++ Src/Twos/Processors/GameActionProcessor.cs
@@ -90,6 +90,9 @@ namespace Twos.Processors
 
                 if (!AnyMovesPossible(state.Board))
                     state.Status = GameStatus.Lost;
+
+                if (HasWon(state.Board))
+                    state.Status = GameStatus.Won;
             }
         }
         
@@ -201,6 +204,16 @@ namespace Twos.Processors
             }
 
             // No moves found
+            return false;
+        }
+
+        private bool HasWon(int[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+                for (int col = 0; col < board.GetLength(1); col++)
+                    if (board[row, col] == 2048)
+                        return true;
+
             return false;
         }
     }
